1ab6bd7e6729d246a49a41cf5dbd944e1283adaa
Denisa Babeii <babeii.denisa@yahoo.ro>
2021-11-04 14:22:09 +0200

    Add validation and menu

--- src/main/java/FAProgram.java
+++ src/main/java/FAProgram.java
@@ -7,14 +7,17 @@ public class FAProgram {
     ArrayList<String> alphabet;
     ArrayList<String> finalStates;
     ArrayList<Triple> transitions;
+    boolean isDFA;
 
     public FAProgram() {
         statesSet = new ArrayList<>();
         alphabet = new ArrayList<>();
         finalStates = new ArrayList<>();
-        transitions = new ArrayList<>(); // !
+        transitions = new ArrayList<>();
+        isDFA = true;
     }
-    private void getElements(String file) throws Exception {
+
+    public void getElements(String file) throws Exception {
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader(file));
@@ -28,6 +31,11 @@ public class FAProgram {
 
                 else if(lineCount == 2) { // if I am on the second line, where the alphabet is enumerated
                     var tempAlphabet = line.split(",");
+                    for (var item:
+                         tempAlphabet) {
+                        if(item.length()>1)
+                            throw new Exception("Alphabet only accepts letters and digits!");
+                    }
                     alphabet.addAll(Arrays.asList(tempAlphabet));
                 }
 
@@ -38,11 +46,17 @@ public class FAProgram {
                 }
 
                 else {
-                    if(transitions.contains(line))
-                        throw new Exception("Duplicate transition!");
-                    var tempTransitions = line.split(",");
-
-                    transitions.add(new Triple(tempTransitions[0],tempTransitions[1],tempTransitions[2]));
+                    var tempTransition = line.split(",");
+                    if(isDFA) { // only check if it is NFA in case this has not been established before
+                        for (var transition :
+                                transitions) {
+                            if (transition.first.equals(tempTransition[0]) && transition.second.equals(tempTransition[1])) {
+                                isDFA = false;
+                                break;
+                            }
+                        }
+                    }
+                    transitions.add(new Triple(tempTransition[0],tempTransition[1],tempTransition[2]));
                 }
 
                 line=reader.readLine();
@@ -54,38 +68,40 @@ public class FAProgram {
         }
     }
 
-    private void printElements() {
-        // print states
+    public void printStates() {
         StringBuilder states = new StringBuilder();
-        for (var state:
-             statesSet) {
-            states.append(state+", ");
+        for (var state :
+                statesSet) {
+            states.append(state + ", ");
         }
-        states.delete(states.length()-2,states.length());
-        System.out.println("Q={"+states.toString()+"}");
+        states.delete(states.length() - 2, states.length());
+        System.out.println("Q={" + states.toString() + "}");
+    }
 
-        // print initial state
-        System.out.println("q0="+statesSet.get(0).toString());
+    public void printInitialState() {
+        System.out.println("q0=" + statesSet.get(0).toString());
+    }
 
-        // print alphabet
+    public void printAlphabet() {
         StringBuilder tempAlphabet = new StringBuilder();
-        for (var item:
+        for (var item :
                 alphabet) {
-            tempAlphabet.append(item+", ");
+            tempAlphabet.append(item + ", ");
         }
-        tempAlphabet.delete(tempAlphabet.length()-2,tempAlphabet.length());
-        System.out.println("Sigma={"+tempAlphabet.toString()+"}");
+        tempAlphabet.delete(tempAlphabet.length() - 2, tempAlphabet.length());
+        System.out.println("Sigma={" + tempAlphabet.toString() + "}");
+    }
 
-        // print transitions
-
+    public void printTransitions() {
         StringBuilder tempTransitions = new StringBuilder();
-        for (var element:
-             transitions) {
-            tempTransitions.append("Delta("+element.first+","+element.second+")="+element.third+"\n");
+        for (var element :
+                transitions) {
+            tempTransitions.append("Delta(" + element.first + "," + element.second + ")=" + element.third + "\n");
         }
         System.out.print(tempTransitions);
+    }
 
-        // print final states
+    public void printFinalStates() {
         StringBuilder tempFinalStates = new StringBuilder();
         for (var finalState:
                 finalStates) {
@@ -93,11 +109,48 @@ public class FAProgram {
         }
         tempFinalStates.delete(tempFinalStates.length()-2,tempFinalStates.length());
         System.out.println("F={"+tempFinalStates.toString()+"}");
-
     }
 
-    public void run(String file) throws Exception {
-        getElements(file);
-        printElements();
+    public void printDFA() {
+       if(isDFA)
+           System.out.println("It is a DFA");
+       else
+           System.out.println("It is a NFA");
+    }
+
+    private String getTransition(String current, String sequenceElem) {
+        for (var transition:
+             transitions) {
+            if(transition.first.equals(current) && transition.second.equals(sequenceElem))
+                return transition.third;
+        }
+        return "";
+    }
+
+    private boolean checkSequence(String sequence) {
+        var current = statesSet.get(0);
+
+        while(sequence.length()!=0) {
+            String sequenceElem = Character.toString(sequence.charAt(0));
+            var transition = getTransition(current,sequenceElem);
+            if(transition.equals(""))
+                return false;
+            else {
+                current = transition;
+                sequence = sequence.substring(1);
+            }
+        }
+        return true;
+    }
+
+    public void printValidity(String sequence) {
+        if(isDFA) {
+            if (checkSequence(sequence))
+                System.out.println("It is valid");
+            else
+                System.out.println("It is not valid");
+        }
+        else
+            System.out.println("It is not a DFA.");
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,58 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void printMenu(FAProgram program) throws IOException {
+        var run = true;
+        while(run) {
+            System.out.println("\n1. Get states\n" +
+                    "2. Get alphabet\n" +
+                    "3. Get transitions\n" +
+                    "4. Get final states\n" +
+                    "5. Get initial state\n" +
+                    "6. Is it a DFA?\n" +
+                    "7. Verify sequence\n" +
+                    "8. Exit\n");
+            Scanner scanner = new Scanner(System.in);
+            var choice = scanner.next();
+            switch (choice) {
+                case "1":
+                    program.printStates();
+                    break;
+                case "2":
+                    program.printAlphabet();
+                    break;
+                case "3":
+                    program.printTransitions();
+                    break;
+                case "4":
+                    program.printFinalStates();
+                    break;
+                case "5":
+                    program.printInitialState();
+                    break;
+                case "6":
+                    program.printDFA();
+                    break;
+                case "7":
+                    var sequence = scanner.next();
+                    program.printValidity(sequence);
+                    break;
+                case "8":
+                    run = false;
+                    break;
+                default:
+                    System.out.println("Please pick an available choice!");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         var program = new FAProgram();
         try {
-            program.run("src/main/resources/FA.in");
+            program.getElements("src/main/resources/FA.in");
+            printMenu(program);
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
